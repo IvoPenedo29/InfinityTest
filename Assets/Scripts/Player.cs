49560af8097b861be56d0e1a67a6d8ac66b0ee6f
IvoPenedo29 <ivoti.98@sapo.pt>
2020-01-13 12:15:01 +0000

    Adicionei um sistema de partículas que segue o input do jogador

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public colors playerInk;
+
+    public GameObject particlePrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 1.5f;
+            var worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(particlePrefab, worldPos, Quaternion.identity);
+        }
+    }
 }
